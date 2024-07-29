5042273c2ab2a6d96e1ca90ea94d62beebb61448
isammittal <152159254+isammittal@users.noreply.github.com>
2024-07-29 20:57:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const faqs = document.querySelectorAll('.faq');
+  
+    faqs.forEach((faq) => {
+      faq.addEventListener('click', () => {
+        faq.classList.toggle('faq-open');
+      });
+    });
+  });
+  
